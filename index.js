56534afceebd4c49ec1af6257f677fa414933377
Orlando Sergio Dominguez <serggio@msn.com>
2016-08-29 12:05:04 -0300

    Versión 0.1: Inicio de proyecto.

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.append('HTTP/1.1','404 Not Found');
+	response.append('HTTP/1.1:','404 Not Found');
+	response.append('Status','404 Not Found');
+	response.append('Conection','close');
+	response.send('');
+});
+
+app.get('/test',function(Rq,Rs){
+	Rs.header("Access-Control-Allow-Origin", "*");
+  	Rs.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	Rs.send('Chequea el manual para implementar esta API.');
 });
 
 app.listen(app.get('port'), function() {
